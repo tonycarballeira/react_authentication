eedb64e9d6f3a78c47b3f52b763002340e54e040
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-28 18:24:21 -0500

    Setup export component in signin.js

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -7,3 +7,21 @@ var {
 	TextInput
 } = React;
 
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<View>
+				<Text>Sign In</Text>
+			</View>
+		)
+	}
+});
+
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
