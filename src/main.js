ffd5df94a205bbb8b91ffb699df1b6163ee637e6
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-28 17:53:15 -0500

    Created main.js component with some simple styling

--- src/main.js
+++ src/main.js
@@ -10,8 +10,16 @@ module.exports = React.createClass({
 	render: function() {
 		return (
 			<View style={styles.container}>
-
+				<Text>I am on both iOS and Android!</Text>
 			</View>
 		)
+	}
+});
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
 	}
 });
