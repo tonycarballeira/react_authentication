b89b31e14250cdb8e3496c5eca68233bc26d212a
Tony Carballeira <tonycarballeira@gmail.com>
2016-01-28 17:49:07 -0500

    Intial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+var React = require('react-native');	
+
+var {
+	View,
+	Text,
+	StyleSheet
+} = React;
+
+module.exports = React.createClass({
+	render: function() {
+		return (
+			<View style={styles.container}>
+
+			</View>
+		)
+	}
+});
